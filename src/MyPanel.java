0a6e8c1662c8692e2f785dce221c557751e1e003
Denis <denispsenicnyh@gmail.com>
2023-03-03 11:01:05 +0300

    Подготовка графической части

---
+++ src/MyPanel.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+    public MyPanel() {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(1500, 700);
+    }
+
+    int x = 0;
+    int y = 0;
+    int width = 100;
+    int height = 100;
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
+
