aab4674fa02372aeaf30c27ae0ae3a7aa2ed0992
Denis <denispsenicnyh@gmail.com>
2023-03-16 17:44:57 +0300

    Вывод семи сегментов

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -11,45 +11,26 @@ public class MyPanel extends JPanel {
 
     int x = 100;
     int y = 100;
-    int width = 50;
-    int height = 100;
-    int step = 30;
-    int delta = 5;
+    int width = 200;
+    int height = 200;
+    int delta = 10;
     private final ArrayList<Rectangle> outlist = new ArrayList<>();
-    public void setSymbols(String parm){
-        String[] arr = parm.replace( "," , "").split("");
-        for (String cifra:arr) {
-            switch (cifra) {
-                case "1" -> {
-                    segment(2);
-                    segment(4);
-                }
-                case "2" -> {
-                    segment(1);
-                    segment(2);
-                    segment(3);
-                    segment(6);
-                    segment(5);
-                }
-                case "3" -> {
-                    segment(1);
-                    segment(2);
-                    segment(3);
-                    segment(4);
-                    segment(6);
-                }
-            }
-            x = x + step + width;
-        }
-
+    public void setSymbols(String ignoredParm){
+        segment(1);
+        segment(2);
+        segment(3);
+        segment(4);
+        segment(5);
+        segment(6);
+        segment(7);
     }
     private void segment(int number){
         switch (number) {
-            case 1 -> outlist.add(new Rectangle(x + delta, y, x+width - delta, y));
-            case 2 -> outlist.add(new Rectangle(x+width, y + delta,x+width, y + (height / 2) - delta));
-            case 3 -> outlist.add(new Rectangle(x + delta, y + height / 2, x+width - delta, y + height / 2));
-            case 4 -> outlist.add(new Rectangle(x+width, y + (height / 2) + delta, x+width, y + height - delta));
-            case 5 -> outlist.add(new Rectangle(x + delta, y + height, x+width - delta, y + height));
+            case 1 -> outlist.add(new Rectangle(x + delta, y, width - delta, y));
+            case 2 -> outlist.add(new Rectangle(width, y + delta, width, y + (height / 2) - delta));
+            case 3 -> outlist.add(new Rectangle(x + delta, y + height / 2, width - delta, y + height / 2));
+            case 4 -> outlist.add(new Rectangle(width, y + (height / 2) + delta, width, y + height - delta));
+            case 5 -> outlist.add(new Rectangle(x + delta, y + height, width - delta, y + height));
             case 6 -> outlist.add(new Rectangle(x, y + (height / 2) + delta, x, y + (height / 2) - delta));
             case 7 -> outlist.add(new Rectangle(x, y + delta, x, y + (height / 2) - delta));
         }
@@ -61,7 +42,8 @@ public class MyPanel extends JPanel {
 
         for (Rectangle rect:outlist){
             g.drawLine(rect.x,rect.y,rect.width,rect.height);
-
         }
-    }
+     }
 }
+
+
